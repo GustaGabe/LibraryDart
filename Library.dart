0224cd4a5e1b1e0c7c739420d0366d5024c11c84
GustaGabe <notscry@outlook.com>
2025-04-14 15:33:52 -0300

    feat: add prints

--- Library.dart
+++ Library.dart
@@ -1,19 +1,34 @@
 void main () {
-  String libraryName = "MyLibrary";
-  int totalBooks = 100;
-  double mensality = 5.99;
-  bool isBookAvaliable;
   List<Map<String, dynamic>> livro = [];
+  String libraryName = "GusLibrary";
+  double mensality = 5.99;
+  bool isBookAvaliable = true;
+
   void adicionarLivro (String titulo, String autor, int ano) {
     livro.add({
       "titulo": titulo,
       "autor": autor,
       "ano": ano,
   });
-  }
+}
+
+  print('====================');
+  print('=====$libraryName=====');
+  print('====================');
+
   adicionarLivro('Gusta', "autor", 2000);
   adicionarLivro('Gusta3', "autor2", 2005);
+  int totalBooks = livro.length;
+  print('Total de livros: $totalBooks');
 
+
+  var imposto = (mensality, totalBooks) {
+    return mensality + totalBooks;
+  };
+
+  print('Imposto: ${imposto(mensality, totalBooks)}');
+  print('Os livros estão disponíveis $isBookAvaliable');
+  print('Livros:');
   livro.forEach((livro) {
     print("Título: ${livro['titulo']}, Autor: ${livro['autor']}, Ano: ${livro['ano']}");
   });
