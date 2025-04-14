d98114381b2358aa26ce01eefc95765abe138647
GustaGabe <notscry@outlook.com>
2025-04-14 15:05:12 -0300

    feat: adding List & Map

--- Library.dart
+++ Library.dart
@@ -3,4 +3,18 @@ void main () {
   int totalBooks = 100;
   double mensality = 5.99;
   bool isBookAvaliable;
+  List<Map<String, dynamic>> livro = [];
+  void adicionarLivro (String titulo, String autor, int ano) {
+    livro.add({
+      "titulo": titulo,
+      "autor": autor,
+      "ano": ano,
+  });
+  }
+  adicionarLivro('Gusta', "autor", 2000);
+  adicionarLivro('Gusta3', "autor2", 2005);
+
+  livro.forEach((livro) {
+    print("Título: ${livro['titulo']}, Autor: ${livro['autor']}, Ano: ${livro['ano']}");
+  });
 }
